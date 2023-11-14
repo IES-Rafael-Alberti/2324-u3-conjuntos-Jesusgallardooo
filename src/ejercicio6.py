bb32a6f7617646861151528b7011545117fd0771
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2023-11-14 08:09:06 +0100

    ª

---
+++ src/ejercicio6.py
@@ -0,0 +1,8 @@
+''' 
+    Dado el conjunto de letras:
+
+vocales = {'a', 'e', 'i', 'o', 'u'}
+
+    Crea un conjunto consonantes que contenga las letras del alfabeto que no son vocales.
+    Crea un conjunto letras_comunes que contenga las letras que están tanto en el conjunto vocales como en el conjunto consonantes.
+'''
